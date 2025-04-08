30419a0b1eafd820f6f0c5ac4b1df44931e31204
MarcelNota <n3gr1t0.mn@gmail.com>
2025-04-08 11:35:04 +0200

    addition of requirements and dependencys

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+# Importing
+
 from flask import Flask
 
-app = Flask()
+app = Flask(__name__)
+
+# Routes (Root route/ Initial page)
+
+@app.route('/')
+def welcome():
+    return 'Bem Vindo'
+
+if __name__ == "__main__":
+    app.run(debug=True)
