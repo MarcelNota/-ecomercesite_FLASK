26f6869899339502c7a8b685d2a4d92eea607de3
MarcelNota <n3gr1t0.mn@gmail.com>
2025-04-23 19:51:53 +0200

    Adiction of Database dependencys

--- app.py
+++ app.py
@@ -4,7 +4,19 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE']
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecomerce.db'
+
+db = SQLAlchemy(app)
+
+# Database Modelatiion
+
+class Product(db.Model):
+    id = db.Colum(db.Integer , primary_key=True)
+    name = db.Colum(db.String(120) , nullable=False)
+    price = db.Colum(db.Float , nullable=False)
+    description = db.Colum(db.Text , nullable=True)
+     
+
 # Routes (Root route/ Initial page)
 
 @app.route('/')
