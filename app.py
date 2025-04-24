bd747a7a7858398e5183dfba9927e2b43a5b4e86
MarcelNota <n3gr1t0.mn@gmail.com>
2025-04-24 04:05:21 +0200

    Addition of routes for POST and Delete (refining them)

--- app.py
+++ app.py
@@ -21,12 +21,34 @@ class Product(db.Model):
 @app.route('/api/products/add', methods=["POST"])
 def add_product():
     data = request.json  # input sent by client
-    if 'name' and 'price' in data:
+    if 'name' in data and 'price' in data:
         product = Product(name=data.get("name" , "Name not found"), price=data.get("price" , "Price not found"), description=data.get("description" , "Description not found"))    # to catch what the cllient typed (keys and values) ex: "name":"tv"
         db.session.add(product) # opens session and adds product
         db.session.commit()
-        return "PRODUCT ADDED SUCCESSFULY"            # 2 ways of getting client data 1st name=data["name"]
-    return jsonfy({"message":"INVALID PRODUCT DATA"}) # and name=data.get("name", "ERROR MESSAGE")
+        return jsonify({"message" : "PRODUCT ADDED SUCCESSFULY" }) 
+    return jsonify({"message" : "INVALID PRODUCT DATA"}), 400  # 400 is code error for invalid inserted data                                                             
+                                  
+                                  
+@app.route("/api/products/delete/<int:product_id>", methods=["DELETE"]) # dentro de <tipoDado e o nomeVariavel> , aspas podem ser duplos como simples
+def delete_Product(product_id):
+    # retrieve product from database
+    # verify if it exists
+    # if exists delete it from database
+    # if dont exist return error 404 "PRODUCT NOT FOUND"
+     
+    product = Product.query.get(product_id)
+    if product:                               # it could be (product != None:)
+        db.session.delete(product)
+        db.session.commit()
+        return jsonify({"message" : "PRODUCT DELETED SUCCESSFULY" }) 
+    return jsonify({"message" : "PRODUCT NOT FOUND IN DATABASE"}), 404  # 404 is code error for not found data
+    
+    
+                                                                  
+             # 2 ways of getting client data 1st name=data["name"]
+             # and name=data.get("name", "ERROR MESSAGE")
+
+
 @app.route('/')
 def welcome():
     return 'Bem Vindo'
